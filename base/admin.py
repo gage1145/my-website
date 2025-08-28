ee9e03936f39a98b42ac7189aef5e7f1617cbeb4
gage1145 <gage.rowden1145@gmail.com>
2025-08-27 20:30:48 -0500

    Updated admin.

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Affiliation, Author, Project, Publication, Skill
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'link', 'created_on')
+    
