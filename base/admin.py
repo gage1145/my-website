e838928e5e5be764d24c52310408adbcfd2f3192
gage1145 <140093724+gage1145@users.noreply.github.com>
2025-09-03 09:47:59 -0500

    Converted the project to a static site. Django was way overkill for this project.

--- base/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Affiliation, Author, Project, Publication, Skill
-
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'link', 'created_on')
-    
--- base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.home),
-    path('api/projects/', views.projects_api, name='projects_api')
-]
