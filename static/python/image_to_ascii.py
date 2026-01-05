f436a6beec092c802c4b0cadf0d7d7f6554f549c
gage1145 <gager@priogen.bio>
2026-01-05 14:24:00 -0600

    Added character depth to the ascii converter rather than having a boolean for two character palettes.

--- static/python/image_to_ascii.py
+++ static/python/image_to_ascii.py
@@ -4,9 +4,9 @@ from js import document
 
 
 
-def resize_image(image, new_width=100, rotate=0):
+def resize_image(image, new_width, rotate):
     width, height = image.size
-    ratio = height / (2.8 * width)
+    ratio = height / (2.2 * width)
     new_height = int(new_width * ratio)
     return image.rotate(rotate, expand=True).resize((new_width, new_height))
 
@@ -19,22 +19,31 @@ def change_contrast(img, level=100):
 def grayify(image):
     return image.convert("L")
 
-def pixels_to_ascii(image, simple=True):
+def pixels_to_ascii(image, depth):
     pixels = image.getdata()
-    if simple:
-        ASCII_CHARS = list("@#$%?*+;:,. ")[::-1]
-        characters = "".join(ASCII_CHARS[p // 22] for p in pixels)
-    else:
-        ASCII_CHARS = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft|(1[?-_+~i!lI;:,^'. ")[::-1]
-        characters = "".join(ASCII_CHARS[p // 5] for p in pixels)
+    
+    if depth == 1:
+        ASCII_CHARS = list(" @")
+    elif depth == 2:
+        ASCII_CHARS = list(" /9@")
+    elif depth == 3:
+        ASCII_CHARS = list(" ^/([9$@")
+    elif depth == 4:
+        ASCII_CHARS = list(" .-^</T(f[j9K$M@")
+    elif depth == 5:
+        ASCII_CHARS = list(" `.-':^;<!/zTJ(if3[5jwk9OK8$0M%@")
+    elif depth == 6:
+        ASCII_CHARS = list(""" `.-':_^=;><!c*/zsTv)J(Fi{fI31t[ne5Yxja]wqkP9d4pOAKXm8D$Bg0MQ%&@""")
+    
+    characters = "".join(ASCII_CHARS[p // (256 // len(ASCII_CHARS))] for p in pixels)
     return characters
 
-def convert_image_from_bytes(image_bytes, new_width=100, contrast=100, simple=True, rotate=0):
+def convert_image_from_bytes(image_bytes, depth, new_width, contrast, rotate):
     image = Image.open(BytesIO(image_bytes))
-    image = resize_image(image, new_width=new_width, rotate=rotate)
+    image = resize_image(image, new_width, rotate)
     image = change_contrast(image, contrast)
     image = grayify(image)
-    ascii_data = pixels_to_ascii(image, simple=simple)
+    ascii_data = pixels_to_ascii(image, depth)
     pixel_count = len(ascii_data)
     ascii_image = "\n".join(
         ascii_data[i:i + new_width]
@@ -55,7 +64,7 @@ async def run(event=None):
 
     width = int(document.getElementById("width").value)
     contrast = int(document.getElementById("contrast").value)
-    simple = document.getElementById("simple").checked
+    depth = int(document.getElementById("depth").value)
     rotate = float(document.getElementById("rotate").value)
 
     buffer = await file.arrayBuffer()
@@ -63,9 +72,9 @@ async def run(event=None):
 
     ascii_img = convert_image_from_bytes(
         image_bytes,
+        depth,
         new_width=width,
         contrast=contrast,
-        simple=simple,
         rotate=rotate
     )
 
