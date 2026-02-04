fd35534a730053fc0214d3510134343c9abde0ac
gage1145 <140093724+gage1145@users.noreply.github.com>
2026-02-04 16:48:14 -0600

    Updated mandelbrot

--- static/javascript/mandelbrot.js
+++ static/javascript/mandelbrot.js
@@ -1,24 +1,24 @@
 const zoomSlider = document.getElementById("zoom");
 const maxIterationsInput = document.getElementById("iterations");
-const startColorSlider = document.getElementById("start-color");
-const endColorSlider = document.getElementById("end-color");
+const hueSlider = document.getElementById("hue");
+const saturationSlider = document.getElementById("saturation");
+const lightnessSlider = document.getElementById("lightness");
+const paletteSlider = document.getElementById("palette-cycles");
+const colorScaleSlider = document.getElementById("color-scale");
 const saveButton = document.getElementById("save");
+
+let maxIterations = maxIterationsInput.value;
+let hue = hueSlider.value;
+let saturation = saturationSlider.value;
+let lightness = lightnessSlider.value;
+let paletteCycles = paletteSlider.value;
+let colorScale = colorScaleSlider.value;
 
 let xOffset = 0;
 let yOffset = 0;
 
-let lastMouseX, lastMouseY;
-let isDragging = false;
-
-let redrawTimeout;
 
 let colorLUT = [];
-
-[zoomSlider, maxIterationsInput, startColorSlider, endColorSlider]
-    .forEach(el => {
-        el.addEventListener("input", () => redraw());
-    });
-
 
 function setup() {
     parentElement = document.getElementById("sketch");
@@ -29,28 +29,23 @@ function setup() {
     canvas.parent("sketch");
 
     pixelDensity(1);
-    colorMode(HSL);
-    noLoop();
+    colorMode(HSL, 360, 100, 100, 255);  
+
+    buildColorLUT();
 
     renderMandelbrot();
 }
 
 function draw() {
-    canvas.style("cursor", "grab");
+    canvas.style("cursor", "pointer");
     renderMandelbrot();
     noLoop();
 }
 
 function renderMandelbrot() {
     let zoom = parseFloat(zoomSlider.value);
-    let startColor = parseFloat(startColorSlider.value);
-    let endColor = parseFloat(endColorSlider.value);
 
     background(0, 0, 6);
-
-    let maxIterations = maxIterationsInput.value;  
-    buildColorLUT(maxIterations, startColor, endColor);
-
 
     let w = 1 / pow(zoom, 2);
     let h = (w * height) / width;
@@ -66,7 +61,6 @@ function renderMandelbrot() {
     let dx = (xMax - xMin) / width;
     let dy = (yMax - yMin) / height;
 
-    // let y = yMin;
     for (let j = 0; j < height; j++) {
         let y = yMin + j * dy;
         let x = xMin;
@@ -89,14 +83,14 @@ function renderMandelbrot() {
 
             let idx = (i + j * width) * 4;
             if (iterations < maxIterations) {
-                let [h, s, l] = colorLUT[iterations];
-                pixels[idx]     = h;
-                pixels[idx + 1] = s;
-                pixels[idx + 2] = l;
+                let [r, g, b] = colorLUT[iterations];
+                pixels[idx]     = r;
+                pixels[idx + 1] = g;
+                pixels[idx + 2] = b;
             } else {
                 pixels[idx]     = 0;
                 pixels[idx + 1] = 0;
-                pixels[idx + 2] = 6;
+                pixels[idx + 2] = 0;
             }
             pixels[idx + 3] = 255;
             x += dx;
@@ -106,9 +100,7 @@ function renderMandelbrot() {
 }
 
 function mouseClicked() {
-    // Only respond if click is inside the canvas
     if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) return;
-
     recenterOnClick(mouseX, mouseY);
     redraw();
 }
@@ -116,32 +108,50 @@ function mouseClicked() {
 function recenterOnClick(px, py) {
     let zoom = parseFloat(document.getElementById("zoom").value);
 
-    // Current view window
     let w = 1 / pow(zoom, 2);
     let h = (w * height) / width;
 
     let xMin = -w / 2 + xOffset;
     let yMin = -h / 2 + yOffset;
 
-    // Map pixel → complex
     let cx = xMin + (px / width) * w;
     let cy = yMin + (py / height) * h;
 
-    // Recenter
     xOffset = cx;
     yOffset = cy;
 }
 
-function buildColorLUT(maxIterations, startHue, endHue) {
+function buildColorLUT() {
     colorLUT.length = maxIterations + 1;
+    colorScale = map(colorScale, 0, 1, 0.001, 0.1);
+    N = paletteCycles;
 
     for (let i = 0; i <= maxIterations; i++) {
-        let t = i / maxIterations;
-        let hue = lerp(startHue, endHue, sqrt(t));
-        let lightness = lerp(0, 100, sqrt(t));
-        colorLUT[i] = [hue, 255, lightness];
+        v = pow(pow(i / maxIterations, colorScale) * N, 1.5) % N;
+        h = v * hue % 360;
+        s = saturation % 100;
+        l = v * lightness % 100;
+        c = color(h, s, l);
+
+        colorLUT[i] = [red(c), green(c), blue(c)];
     }
 }
+
+zoomSlider.addEventListener("input", () => redraw());
+
+[maxIterationsInput, hueSlider, saturationSlider, lightnessSlider, paletteSlider, colorScaleSlider]
+    .forEach(el => {
+        el.addEventListener("input", () => {
+            hue = hueSlider.value;
+            saturation = saturationSlider.value;
+            lightness = lightnessSlider.value;    
+            maxIterations = maxIterationsInput.value;
+            paletteCycles = paletteSlider.value;
+            colorScale = colorScaleSlider.value;
+            buildColorLUT(maxIterations);
+            redraw();
+        })
+    })
 
 saveButton.addEventListener("click", () => save("mandelbrot.png"));
 
