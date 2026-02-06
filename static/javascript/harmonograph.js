e9fda0bb7b795cb0fd32ba2e3088604bd22b60cb
gage1145 <140093724+gage1145@users.noreply.github.com>
2026-02-06 12:01:26 -0600

    Added a harmonograph tool.

---
+++ static/javascript/harmonograph.js
@@ -0,0 +1,123 @@
+// const saveButton = document.getElementById("save");
+
+const f1Slider = document.getElementById("f1-slider");
+const f2Slider = document.getElementById("f2-slider");
+const f3Slider = document.getElementById("f3-slider");
+const f4Slider = document.getElementById("f4-slider");
+
+const a1Slider = document.getElementById("a1-slider");
+const a2Slider = document.getElementById("a2-slider");
+const a3Slider = document.getElementById("a3-slider");
+const a4Slider = document.getElementById("a4-slider");
+
+const p1Slider = document.getElementById("p1-slider");
+const p2Slider = document.getElementById("p2-slider");
+const p3Slider = document.getElementById("p3-slider");
+const p4Slider = document.getElementById("p4-slider");
+
+const d1Slider = document.getElementById("d1-slider");
+const d2Slider = document.getElementById("d2-slider");
+const d3Slider = document.getElementById("d3-slider");
+const d4Slider = document.getElementById("d4-slider");
+
+var f1
+var f2
+var f3
+var f4
+var a1
+var a2
+var a3
+var a4
+var p1
+var p2
+var p3
+var p4
+var d1
+var d2
+var d3
+var d4
+
+let iterations = 100000;
+
+let dt = 0.001;
+
+function setup() {
+    parentElement = document.getElementById("sketch");
+    width = parentElement.clientWidth;
+    height = width * 0.75;
+    canvas = createCanvas(width, height);
+    canvas.parent("sketch");
+    pixelDensity(1);
+    background(0);
+}
+
+function draw() {
+    translate(width / 2, height / 2);
+    background(0);
+    renderHarmonograph();
+    noLoop();
+}
+
+function renderHarmonograph () {
+    f1 = random(0,parseFloat(f1Slider.value));
+    f2 = random(0,parseFloat(f2Slider.value));
+    f3 = random(0,parseFloat(f3Slider.value));
+    f4 = random(0,parseFloat(f4Slider.value));
+    a1 = random(0,parseFloat(a1Slider.value));
+    a2 = random(0,parseFloat(a2Slider.value));
+    a3 = random(0,parseFloat(a3Slider.value));
+    a4 = random(0,parseFloat(a4Slider.value));
+    p1 = random(0,parseFloat(p1Slider.value));
+    p2 = random(0,parseFloat(p2Slider.value));
+    p3 = random(0,parseFloat(p3Slider.value));
+    p4 = random(0,parseFloat(p4Slider.value));
+    d1 = random(0,parseFloat(d1Slider.value));
+    d2 = random(0,parseFloat(d2Slider.value));
+    d3 = random(0,parseFloat(d3Slider.value));
+    d4 = random(0,parseFloat(d4Slider.value));
+    let t = 0;
+    noFill();
+    stroke(149, 255, 175);
+    drawingContext.shadowBlur = 10;
+    drawingContext.shadowColor = color(149, 255, 175);
+    strokeWeight(3);
+    beginShape();
+    for (let i = 0; i < iterations; i++) {
+        x = a1 * sin(t * f1 + p1) * exp(-d1 * t) + a2 * sin(t * f2 + p2) * exp(-d2 * t);
+        y = a3 * sin(t * f3 + p3) * exp(-d3 * t) + a4 * sin(t * f4 + p4) * exp(-d4 * t);
+        vertex(x, y);
+        t += dt;
+    }
+    endShape();
+}
+
+[
+    f1Slider, f2Slider, f3Slider, f4Slider,
+    a1Slider, a2Slider, a3Slider, a4Slider,
+    p1Slider, p2Slider, p3Slider, p4Slider,
+    d1Slider, d2Slider, d3Slider, d4Slider,
+].forEach(el => {
+    el.addEventListener("input", () => {
+        f1 = parseFloat(f1Slider.value);
+        f2 = parseFloat(f2Slider.value);
+        f3 = parseFloat(f3Slider.value);
+        f4 = parseFloat(f4Slider.value);
+        a1 = parseFloat(a1Slider.value);
+        a2 = parseFloat(a2Slider.value);
+        a3 = parseFloat(a3Slider.value);
+        a4 = parseFloat(a4Slider.value);
+        p1 = parseFloat(p1Slider.value);
+        p2 = parseFloat(p2Slider.value);
+        p3 = parseFloat(p3Slider.value);
+        p4 = parseFloat(p4Slider.value);
+        d1 = parseFloat(d1Slider.value);
+        d2 = parseFloat(d2Slider.value);
+        d3 = parseFloat(d3Slider.value);
+        d4 = parseFloat(d4Slider.value);
+        redraw();
+    })
+
+})
+
+// saveButton.addEventListener("click", () => save("harmonograph.png"));
+
