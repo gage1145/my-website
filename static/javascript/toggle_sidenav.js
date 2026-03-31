3485460991208308c1734539ff2801ae5cd04eda
gage1145 <gage.rowden1145@gmail.com>
2026-03-31 01:58:25 -0500

    Cleaned up some old stuff I'm not using anymore.

--- static/javascript/toggle_sidenav.js
+++
@@ -1,8 +0,0 @@
-export async function toggleSidenav() {
-    var sidenav = document.getElementById("sidenav");
-    if (sidenav.style.display === "block") {
-        sidenav.style.display = "none";
-    } else {
-        sidenav.style.display = "block";
-    }
-} 
