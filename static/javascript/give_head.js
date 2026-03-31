ffdb6dbec4a3a95a6b152ce7f5f84c2ff19f900e
gage1145 <gage.rowden1145@gmail.com>
2026-03-31 02:01:33 -0500

    I wish I never made this function.

--- static/javascript/give_head.js
+++ static/javascript/give_head.js
@@ -5,17 +5,7 @@ export function give_head() {
     utf.charset = "UTF-8";
     head.appendChild(utf);
     
-    var viewport = document.createElement('meta');
-    viewport.name = "viewport";
-    viewport.content = "width=device-width, initial-scale=1.0";
-    head.appendChild(viewport);
-    
     var title = document.createElement("title");
     title.innerHTML = "Gage Rowden";
     head.appendChild(title);
-    
-    var favicon = document.createElement("link");
-    favicon.href = "static/images/favicon.ico";
-    favicon.rel = "icon";
-    head.appendChild(favicon);
 }
