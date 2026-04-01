09f1ad29100f1561c506ba4796917632f86ffc8c
gage1145 <gage.rowden1145@gmail.com>
2026-03-31 22:32:49 -0500

    Fixed the shortcut highllighting.

--- static/javascript/double_click.js
+++ static/javascript/double_click.js
@@ -1,16 +1,25 @@
+function clearShortcutHighlights() {
+    document.querySelectorAll('.shortcut').forEach(s => {
+        s.style.backgroundColor = '';
+        s.style.color = '';
+    });
+}
+
 export function doubleClickToOpen() {
     const shortcuts = document.querySelectorAll('.shortcut');
     shortcuts.forEach(shortcut => {
         shortcut.addEventListener('click', (e) => {
             e.preventDefault();
-            var id = shortcut.id;
-            var element = document.getElementById(id);
-            element.style.backgroundColor = '#000080';
-            element.style.color = '#fff';
+            e.stopPropagation();
+            clearShortcutHighlights();
+            shortcut.style.backgroundColor = '#000080';
+            shortcut.style.color = '#fff';
         });
         shortcut.addEventListener('dblclick', (e) => {
             e.preventDefault();
             window.location.href = shortcut.href;
         });
     });
+
+    document.addEventListener('click', clearShortcutHighlights);
 }
