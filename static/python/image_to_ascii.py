a3ec33a8496d7f1cb344d941afcbc6d362023dca
gage1145 <gage.rowden1145@gmail.com>
2026-04-27 06:45:59 -0500

    Refactored the image to ascii script.

--- static/python/image_to_ascii.py
+++ static/python/image_to_ascii.py
@@ -18,39 +18,45 @@ def change_contrast(img, level=100):
         return 128 + factor * (c - 128)
     return img.point(contrast)
 
-def grayify(image, edge_behavior: str):
-    print("Edge Behavior = ", edge_behavior)
-    if edge_behavior == "edge-none":
-        return image.convert("L")
-    if edge_behavior == "edge-detect":
-        return image.convert("L").filter(ImageFilter.FIND_EDGES)
-    if edge_behavior == "edge-enhance":
-        return image.convert("L").filter(ImageFilter.EDGE_ENHANCE)
+def grayify(image):
+    return image.convert("L")
+    
+def detect_edges(image, edge_behavior: str):
+    behaviors = {
+        "edge-detect": ImageFilter.FIND_EDGES,
+        "edge-enhance": ImageFilter.EDGE_ENHANCE,
+        "edge-none": None
+    }
+    behavior = behaviors.get(edge_behavior)
+
+    if behavior: return image.filter(behavior)
+    return image
 
 def pixels_to_ascii(image, depth):
     pixels = image.getdata()
-    
-    if depth == 1:
-        ASCII_CHARS = list(" @")
-    elif depth == 2:
-        ASCII_CHARS = list(" /9@")
-    elif depth == 3:
-        ASCII_CHARS = list(" ^/([9$@")
-    elif depth == 4:
-        ASCII_CHARS = list(" .-^</T(f[j9K$M@")
-    elif depth == 5:
-        ASCII_CHARS = list(" `.-':^;<!/zTJ(if3[5jwk9OK8$0M%@")
-    elif depth == 6:
-        ASCII_CHARS = list(""" `.-':_^=;><!c*/zsTv)J(Fi{fI31t[ne5Yxja]wqkP9d4pOAKXm8D$Bg0MQ%&@""")
-    
+    ascii_list = [
+        list(" @"),
+        list(" /9@"),
+        list(" ^/([9$@"),
+        list(" .-^</T(f[j9K$M@"),
+        list(" `.-':^;<!/zTJ(if3[5jwk9OK8$0M%@"),
+        list(""" `.-':_^=;><!c*/zsTv)J(Fi{fI31t[ne5Yxja]wqkP9d4pOAKXm8D$Bg0MQ%&@""")
+    ]
+    ASCII_CHARS = ascii_list[depth - 1]
     characters = "".join(ASCII_CHARS[p // (256 // len(ASCII_CHARS))] for p in pixels)
     return characters
 
-def convert_image_from_bytes(image_bytes, depth, new_width, contrast, edge_behavior):
+def convert_image_from_bytes(image_bytes, 
+                             depth, 
+                             new_width, 
+                             contrast, 
+                             edge_behavior):
+    
     image = Image.open(BytesIO(image_bytes))
     image = resize_image(image, new_width)
     image = change_contrast(image, contrast)
-    image = grayify(image, edge_behavior=edge_behavior)
+    image = grayify(image)
+    image = detect_edges(image, edge_behavior)
     ascii_data = pixels_to_ascii(image, depth)
     pixel_count = len(ascii_data)
     ascii_image = "\n".join(
