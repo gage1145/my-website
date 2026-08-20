1d1ae2940ff3355fe744410c6bbb056190ce4d7c
gage1145 <gage.rowden1145@gmail.com>
2026-08-19 19:37:09 -0500

    Built site for gh-pages

--- static/javascript/file_explorer.js
+++ static/javascript/file_explorer.js
@@ -110,7 +110,7 @@ function showContent(bodyEl, nodeId, data) {
 
     items.forEach(item => {
         const li = document.createElement('li');
-        li.className = 'shortcut explorer-shortcut';
+        li.className = 'explorer-shortcut';
         li.dataset.window = item.id;
         li.innerHTML = `<a style="text-decoration: none; color: black; text-wrap: inherit; cursor: default;" href="${item.link}"><img src="${ICON_BASE}${item.largeIcon}" alt="">${item.title}</a>`;
         contentPane.appendChild(li);
